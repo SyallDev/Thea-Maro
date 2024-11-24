5e183ad04ba739f611e499462b1db23ec68b7d4a
jiggy <>
2024-11-24 21:54:37 +0000

    feat: add cancel and submit order buttons to order process, no logic implemented yet

--- ui/order_form.py
+++ ui/order_form.py
@@ -1,7 +1,6 @@
 import discord
 from discord import ui
 
-
 class Dropdown(ui.Select):
     def __init__(self, id, placeholder, itemName, maxValue):
         formOptions = []
@@ -9,9 +8,32 @@ class Dropdown(ui.Select):
             formOptions.append(discord.SelectOption(label="{} {}".format(str(i), itemName)))
         super().__init__(custom_id=id, placeholder=placeholder, options=formOptions)
 
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Done")
+
+class SubmitOrderButton(ui.Button):
+    def __init__(self):
+        super().__init__()
+        self.label = "Submit order"
+        self.style = discord.ButtonStyle.primary
+
+
+class CancelOrderButton(ui.Button):
+    def __init__(self):
+        super().__init__()
+        self.label = "Cancel order"
+        self.style = discord.ButtonStyle.danger
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.channel.delete(reason="Order cancelled by client")
+
 class DropdownView(ui.View):
     def __init__(self):
         super().__init__()
 
         self.add_item(Dropdown("btAmount", "How many BT?", "BT", 4))
         self.add_item(Dropdown("locoAmount", "How many locomotives?", "Locomotive(s)", 2))
+        self.add_item(Dropdown("flatbedAmount", "How many flatbed cars?", "Flatbed(s)", 14))
+
+        self.add_item(CancelOrderButton())
+        self.add_item(SubmitOrderButton())
